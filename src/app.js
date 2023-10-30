2567484af5ad5da8d828c5e8f397ce5d7e19e86d
Aadith <aadithmanoj1@gmail.com>
2023-10-31 00:49:24 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -4,15 +4,28 @@ import mongoose from "mongoose";
 import adminRoutes from './routes/adminRoutes'; 
 import studentRoutes from './routes/studentRoutes';
 import taskRoutes from './routes/taskRoutes'; //need to create a file
+import { initializeApp } from 'firebase-admin/app';
 
 const app = express();
 app.use(bodyParser.json());
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 const db = mongoose.connection;
 mongoose.connect('mongodb+srv://aadithmanoj1:32OOqa27fRGwAWe5@cluster0.kfkllsc.mongodb.net/', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeilYGI0BA8PscDdzT0c4SapSsbzd4JgY",
+  authDomain: "student-management-v2.firebaseapp.com",
+  projectId: "student-management-v2",
+  storageBucket: "student-management-v2.appspot.com",
+  messagingSenderId: "790493519838",
+  appId: "1:790493519838:web:472d2affdf4376b6c3c74f",
+  measurementId: "G-7NV40SM4X4"
+};
 
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
