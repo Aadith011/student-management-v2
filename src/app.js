4f0efff37b7fb8b8e0ecf7f0d2809737dd3b6d0c
Aadith <aadithmanoj1@gmail.com>
2023-11-04 00:41:54 +0530

    updated files

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import cron from 'node-cron';
 import adminRoutes from './routers/adminRoutes.js';
 import studentRoutes from './routers/studentRoutes.js';
 import authRoutes from './routers/authRoutes.js';
+import Task from './models/taskModel.js';
 
 const app = express();
 app.use(bodyParser.json());
@@ -19,6 +20,26 @@ const port = process.env.PORT || 5000;
 app.use('/admin', adminRoutes);
 app.use('/student', studentRoutes);
 app.use('/auth', authRoutes);
+
+//cron jobs
+cron.schedule('* * * * *', async () => {
+  try {
+    const currentTime = new Date();
+
+    const overdueTasks = await Task.find({
+      status: 'pending',
+      dueTime: { $lt: currentTime },
+    });
+
+    for (const task of overdueTasks) {
+      task.status = 'overdue';
+      await task.save();
+      console.log(`Task with ID ${task._id} is overdue for ${task.studentName}`);
+    }
+  } catch (error) {
+    console.error('Cron job error:', error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
